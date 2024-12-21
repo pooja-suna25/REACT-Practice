306edc1bcdb0b6b97bf9ca05cfa2b8642c5e7df7
Pooja suna <poojasuna.2001@gmail>
2024-12-21 21:41:15 +0530

    creating react app ,displaying Hello World

---
+++ firstrepo/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hello from "./Hello";
+import WelcomeMessage from "./WelcomeMessage";
+import Name from "./Name";
+import Roll from "./Roll";
+import Hobby from "./Hobby";
+import Postal from "./Address";
+function App() {
+  return (
+    <>
+      Hello world
+    </>
+  )
+}
+export default App;
