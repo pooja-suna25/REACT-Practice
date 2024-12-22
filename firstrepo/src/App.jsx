b7fd65e66a48d3d6039e92652dc3052fa1319db5
Pooja suna <poojasuna.2001@gmail>
2024-12-22 13:55:37 +0530

    Displaying messages using different components

--- firstrepo/src/App.jsx
+++ firstrepo/src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Hello from "./Hello";
 import WelcomeMessage from "./WelcomeMessage";
-import Name from "./Name";
-import Roll from "./Roll";
-import Hobby from "./Hobby";
-import Postal from "./Address";
+
+
 function App() {
   return (
     <>
-      Hello world
+    <Hello />
+    <WelcomeMessage />
     </>
   )
 }
---
+++ firstrepo/src/WelcomeMessage.jsx
@@ -0,0 +1,4 @@
+function WelcomeMessage(){
+    return <h1>Welcome to react </h1>;
+  }
+  export default WelcomeMessage;
